2d0c72f6e2fb634f17f3e9a7e3eb1a8509a65e4e
puchevcp <123635253+puchevcp@users.noreply.github.com>
2024-05-22 09:40:50 -0300

    modifico usuario

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/puchevcp/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('puchevcp')
+            node / gitConfigEmail('puche.vcp@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('puchevcp/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
